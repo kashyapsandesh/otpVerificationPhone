765f9b2b4be734505ef783fa6d95ae9c08d1d425
kashyapsandesh <sandeshghimire202@gmail.com>
2023-06-04 20:46:20 +0545

    final commit

---
+++ android/app/src/main/kotlin/com/example/otpverificationfirebase/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.otpverificationfirebase
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:otpverificationfirebase/screens/register_screen.dart';
+import 'package:provider/provider.dart';
+
+import '../provider/auth_provider.dart';
+import '../widgets/custom_button.dart';
+import 'home_screen.dart';
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final ap = Provider.of<AuthProvider>(context, listen: false);
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+            child: Column(
+              children: [
+                Image.asset(
+                  'assets/image1.png',
+                  height: 300,
+                ),
+                const Text(
+                  "Let's get started",
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 10),
+                const Text(
+                  "Never a better time than now to start.",
+                  style: TextStyle(
+                    fontSize: 14,
+                    color: Colors.black38,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+                SizedBox(
+                  width: double.infinity,
+                  height: 50,
+                  child: CustomButton(
+                   onPressed: () async {
+                      if (ap.isSignedIn == true) {
+                        await ap.getDataFromSP().whenComplete(
+                              () => Navigator.pushReplacement(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => const HomeScreen(),
+                                ),
+                              ),
+                            );
+                      } else {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => const RegisterScreen(),
+                          ),
+                        );
+                      }
+                    },
+                    text: "Get started",
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
